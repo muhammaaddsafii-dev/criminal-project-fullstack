a70ad17d7f9b1e774c19646ab801876c4c0c8d34
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2025-12-08 14:37:20 +0700

    add-Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   turbopack: {}, // Tambahkan ini untuk silence warning
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {}, // Tambahkan ini untuk silence warning
+  turbopack: {},
+  output: 'standalone',
 };
 
 export default nextConfig;
