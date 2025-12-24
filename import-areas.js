eeac668b4d051b0a989bf98ee0daaa1ae4df0e40
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2025-12-24 08:12:55 +0700

    update import data area

--- import-areas.js
+++ import-areas.js
@@ -1,5 +1,9 @@
-const { Client } = require('pg');
-const { geoJsonData } = require('./dist/mockData'); // sesuaikan path-nya
+const {
+  Client
+} = require('pg');
+const {
+  geoJsonData
+} = require('./dist/areaData'); // sesuaikan path-nya
 
 async function main() {
   const client = new Client({
@@ -7,7 +11,7 @@ async function main() {
     port: 5432,
     user: 'postgres',
     password: '1sampai8',
-    database: 'criminaldb',
+    database: 'kriminalitas_db',
   });
 
   await client.connect();
@@ -16,40 +20,57 @@ async function main() {
     const p = f.properties;
     const geomJson = JSON.stringify(f.geometry); // langsung GeoJSON
 
+    // await client.query(
+    //   `
+    //   INSERT INTO areas (
+    //     id, metadata, srs_id, wadmkc, wadmkd, wadmkk, wadmpr, uupp,
+    //     luas, kecamatan_id, desa_id, geom
+    //   )
+    //   VALUES (
+    //     $1,$2,$3,$4,$5,$6,$7,$8,$9,
+    //     $10,$11, ST_SetSRID(ST_GeomFromGeoJSON($12), 4326)
+    //   )
+    //   ON CONFLICT (id) DO NOTHING
+    //   `,
+    //   [
+    //     p.id,
+    //     p.METADATA,
+    //     p.SRS_ID,
+    //     p.WADMKC,
+    //     p.WADMKD,
+    //     p.WADMKK,
+    //     p.WADMPR,
+    //     p.UUPP,
+    //     parseFloat(p.LUAS),
+    //     p.kecamatan_id,
+    //     p.desa_id,
+    //     geomJson,
+    //   ]
+    // );
     await client.query(
       `
-      INSERT INTO areas (
-        id, namobj, metadata, srs_id, wadmkc, wadmkd, wadmkk, wadmpr, uupp,
-        luas, pddk_lk, pddk_pr, jmlh_pddk, kpdt_pddk,
-        name, crime_count, crime_rate, color, geom
-      )
-      VALUES (
-        $1,$2,$3,$4,$5,$6,$7,$8,$9,
-        $10,$11,$12,$13,$14,
-        $15,$16,$17,$18,
-        ST_SetSRID(ST_GeomFromGeoJSON($19), 4326)
-      )
-      ON CONFLICT (id) DO NOTHING
-      `,
+  INSERT INTO areas (
+    external_id, metadata, srs_id, wadmkc, wadmkd, wadmkk, wadmpr, uupp,
+    luas, kecamatan_id, desa_id, geom
+  )
+  VALUES (
+    $1,$2,$3,$4,$5,$6,$7,$8,$9,
+    $10,$11, ST_SetSRID(ST_GeomFromGeoJSON($12), 4326)
+  )
+  ON CONFLICT (external_id) DO NOTHING
+  `,
       [
-        p.id,
-        p.NAMOBJ,
-        p.METADATA,
-        p.SRS_ID,
-        p.WADMKC,
-        p.WADMKD,
-        p.WADMKK,
-        p.WADMPR,
-        p.UUPP,
-        parseFloat(p.LUAS),
-        parseInt(p.Pddk_Lk || '0'),
-        parseInt(p.Pddk_Pr || '0'),
-        parseInt(p.Jmlh_Pddk || '0'),
-        parseFloat(p.Kpdt_Pddk || '0'),
-        p.name,
-        p.crimeCount,
-        p.crimeRate,
-        p.color,
+        String(p.id),
+        p.METADATA || null,
+        p.SRS_ID || null,
+        p.WADMKC || null,
+        p.WADMKD || null,
+        p.WADMKK || null,
+        p.WADMPR || null,
+        p.UUPP || null,
+        p.LUAS ? parseFloat(p.LUAS) : null,
+        parseInt(p.kecamatan_id),
+        parseInt(p.desa_id),
         geomJson,
       ]
     );
